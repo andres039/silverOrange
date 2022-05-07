0490fde1494de24fcad3ad5cb97399b89d9d27f1
Andres <andres039039@hotmail.com>
2022-05-07 16:41:44 -0400

    displays a different list according to the button pressed

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -13,35 +13,43 @@ export function App() {
       creationDate: '',
     },
   ]);
+
+  const [displayRepos, setDisplayRepos] = useState([]);
+  const [buttons, setButtons] = useState([]);
   const fetchRepos = async () =>
     await fetch('http://localhost:4000/repos', { mode: 'cors' });
+
   useEffect(() => {
     const getRepos = async () => {
-      const data = await fetchRepos();
-      const reposData = await data.json();
-      const reposDataInOrder = await reposData.sort((a, b) =>
-        a.created_at < b.created_at ? 1 : -1
-      );
-      setRepos(reposDataInOrder);
+      try {
+        const data = await fetchRepos();
+        if (!data.ok) {
+          throw new Error();
+        }
+        const reposData = await data.json();
+        const reposDataInOrder = reposData.sort((a, b) =>
+          a.created_at < b.created_at ? 1 : -1
+        );
+        console.log(reposDataInOrder);
+        setRepos(reposDataInOrder);
+        const languages = await reposData.reduce((acc, cv) => {
+          if (!acc.includes(cv.language)) {
+            acc.push(cv.language);
+          }
+          return acc;
+        }, []);
+        setButtons(languages);
+        setDisplayRepos(reposDataInOrder);
+      } catch (err) {
+        console.error('message', err);
+      }
     };
     getRepos();
   }, []);
 
-  const buttons = repos.reduce(
-    (acc, cv) => {
-      if (!acc.includes(cv.language)) {
-        acc.push(cv.language);
-      }
-      return acc;
-    },
-    [repos]
-  );
-
-  buttons.shift();
-
   const handleClick = (language) => {
     const filterByLanguage = repos.filter((repo) => repo.language === language);
-    setRepos(filterByLanguage);
+    setDisplayRepos(filterByLanguage);
   };
   return (
     <div className="App">
@@ -54,7 +62,7 @@ export function App() {
         ))}
       </Group>
       <Container sx={{ width: '100vw' }}>
-        {repos.map((repo, index) => (
+        {displayRepos.map((repo, index) => (
           <ReposCard key={index} repo={repo} />
         ))}
       </Container>
